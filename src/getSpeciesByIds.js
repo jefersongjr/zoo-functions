b26875ebcdf8806052a2a8660394b32be38b6085
jefersongjr <jeferson_gjr@hotmail.com>
2022-04-14 17:33:23 -0300

    Requisito 1 - dois tests validados

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,13 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getSpeciesByIds(...ids) {
-  // seu código aqui
+  if (ids.length === 0) {
+    return [];
+  }
+  if (ids.length === 1) {
+    return species.filter((animal) => animal.id === ids[0]);
+  }
 }
 
 module.exports = getSpeciesByIds;
