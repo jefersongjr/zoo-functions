700f826c6970d741b12f29d030095cf444e515a6
jefersongjr <jeferson_gjr@hotmail.com>
2022-04-15 00:36:08 -0300

    refatorando o requisito 1

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,13 +1,9 @@
 const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(...ids) {
-  if (ids.length === 0) {
-    return [];
-  }
-  if (ids.length === 1) {
-    return species.filter((animal) => animal.id === ids[0]);
-  }
-}
+const getSpeciesByIds = (...ids) => {
+  const speciesId = species.filter((specie) => ids.includes(specie.id));
+  return speciesId;
+};
 
 module.exports = getSpeciesByIds;
