40346575eda8e8f2bf1361001d9b6c747c926d18
jefersongjr <jeferson_gjr@hotmail.com>
2022-04-15 18:47:32 -0300

    travado requisito 4

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -2,11 +2,20 @@ const { employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function isManager(id) {
-  const managersList = employees.filter((manager) => manager.id === id);
-  const someManager = managersList.some((manager1) => manager1.managers.length > 0);
+  const someManager = employees.some((manager1) => manager1.managers.includes(id));
   return someManager;
 }
 function getRelatedEmployees(managerId) {
+  if (isManager(managerId) === false) {
+    throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+  }
+  const managerRelated = employees.filter((person) => person.managers.includes(managerId));
+  const retorno = employees.map((names)=>{
+    const arrayEmployees = managerRelated.find((name) => name.firstName === names.firstName);
+
+    return ];
+  });
 }
+console.log(getRelatedEmployees('fdb2543b-5662-46a7-badc-93d960fdc0a8'));
 
 module.exports = { isManager, getRelatedEmployees };
