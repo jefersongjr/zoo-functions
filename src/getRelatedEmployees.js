d5a5d957359459190913a73ebc9519e2cd1dfb76
jefersongjr <jeferson_gjr@hotmail.com>
2022-04-15 15:51:33 -0300

    implementando a funçao is Manager

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,12 @@
+const { employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function isManager(id) {
-  // seu código aqui
+  const managersList = employees.filter((manager) => manager.id === id);
+  const someManager = managersList.some((manager1) => manager1.managers.length > 0);
+  return someManager;
 }
-
 function getRelatedEmployees(managerId) {
-  // seu código aqui
 }
 
 module.exports = { isManager, getRelatedEmployees };
