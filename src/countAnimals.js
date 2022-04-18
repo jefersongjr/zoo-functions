c3cd194068bb707657b180f034d660f48185da91
jefersongjr <jeferson_gjr@hotmail.com>
2022-04-18 16:58:58 -0300

    travado ainda no requisito 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,31 +1,16 @@
 const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-const createObject = (filter) => filter.map((specie) => ({
-  name: specie.name,
-  specieQuant: specie.residents.length,
-}));
-
 function countAnimals(animal = { specie: '', sex: '' }) {
+  const animals = species.filter((name) => name.name === animal.specie);
+  const animalForSex = animals[0].residents;
+  const sexOfAnimal = animalForSex.filter((f) => f.sex === animal.sex);
+  if (animal.specie !== '' && animal.sex === undefined) {
+    return animalForSex.length;
+  }
   if (animal.specie !== '' && animal.sex !== '') {
-    const specieSexFilter = species.filter((specie) => specie.residents[0].sex === animal.sex
-    && specie.name === animal.specie);
-    const animalSex = createObject(specieSexFilter);
-    return animalSex[0];
+    return sexOfAnimal.length;
   }
-  if (animal.specie !== undefined) {
-    const speciesFilter = species.filter((specie) => specie.name === animal.specie);
-    const animalAllSex = createObject(speciesFilter);
-    return animalAllSex[0].specieQuant;
-  }
-  const allAnimals = species.map((specie) => ({
-    name: specie.name,
-    specieQuant: specie.residents.length,
-  }));
-  const objectAnimals = allAnimals.reduce((obj, curr) => (
-    { ...obj, [curr.name]: curr.specieQuant }), {});
-  return objectAnimals;
 }
 
 console.log(countAnimals({ specie: 'elephants', sex: 'male' }));
-module.exports = countAnimals;
