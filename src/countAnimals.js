11ed8c735145c3b73d08dea9bee4b1164b5f5061
jefersongjr <jeferson_gjr@hotmail.com>
2022-04-18 13:51:06 -0300

    requisito 5 , refatorando

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,24 +1,31 @@
 const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
+const createObject = (filter) => filter.map((specie) => ({
+  name: specie.name,
+  specieQuant: specie.residents.length,
+}));
+
 function countAnimals(animal = { specie: '', sex: '' }) {
-  /*todos os animais*/
+  if (animal.specie !== '' && animal.sex !== '') {
+    const specieSexFilter = species.filter((specie) => specie.residents[0].sex === animal.sex
+    && specie.name === animal.specie);
+    const animalSex = createObject(specieSexFilter);
+    return animalSex[0];
+  }
+  if (animal.specie !== undefined) {
+    const speciesFilter = species.filter((specie) => specie.name === animal.specie);
+    const animalAllSex = createObject(speciesFilter);
+    return animalAllSex[0].specieQuant;
+  }
   const allAnimals = species.map((specie) => ({
     name: specie.name,
     specieQuant: specie.residents.length,
   }));
   const objectAnimals = allAnimals.reduce((obj, curr) => (
     { ...obj, [curr.name]: curr.specieQuant }), {});
-    /*animais com filtro de especie*/
-  const speciesFilter = species.filter((specie) => specie.name === animal.specie);
-  const animalAllSex = speciesFilter.map((specie) => ({
-    name: specie.name,
-    specieQuant: specie.residents.length,
-  }));
-  const specieSexFilter = speciesFilter.filter((specie) => specie.residents.sex === animal.sex);
- 
-  return animalAllSex[0];
+  return objectAnimals;
 }
 
-console.log(countAnimals({ specie: 'penguins' }));
+console.log(countAnimals({ specie: 'elephants', sex: 'male' }));
 module.exports = countAnimals;
