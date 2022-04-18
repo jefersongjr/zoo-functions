0c22ed552a5f79d3dbfefb856a6ab5e6b6033e35
jefersongjr <jeferson_gjr@hotmail.com>
2022-04-18 18:36:23 -0300

    refatorando o requisito 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -2,15 +2,16 @@ const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function countAnimals(animal = { specie: '', sex: '' }) {
-  const animals = species.filter((name) => name.name === animal.specie);
-  const animalForSex = animals[0].residents;
+  const animals = species.find((name) => name.name === animal.specie);
+  const animalForSex = animals.residents.filter((obj, curr) => (
+    { ...obj, curr: curr.sex }), {});
   const sexOfAnimal = animalForSex.filter((f) => f.sex === animal.sex);
-  if (animal.specie !== '' && animal.sex === undefined) {
+  if (animal.specie !== undefined && animal.sex === undefined) {
     return animalForSex.length;
-  }
-  if (animal.specie !== '' && animal.sex !== '') {
+  } if (animal.specie !== undefined && animal.sex !== undefined) {
     return sexOfAnimal.length;
   }
+
 }
 
 console.log(countAnimals({ specie: 'elephants', sex: 'male' }));
