5797df04f0757eca70478686f655e45c9bfb0070
jefersongjr <jeferson_gjr@hotmail.com>
2022-04-18 11:50:17 -0300

    requisito 5 , todos os animais e animais de uma especia

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,24 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function countAnimals(animal) {
-  // seu código aqui
+function countAnimals(animal = { specie: '', sex: '' }) {
+  /*todos os animais*/
+  const allAnimals = species.map((specie) => ({
+    name: specie.name,
+    specieQuant: specie.residents.length,
+  }));
+  const objectAnimals = allAnimals.reduce((obj, curr) => (
+    { ...obj, [curr.name]: curr.specieQuant }), {});
+    /*animais com filtro de especie*/
+  const speciesFilter = species.filter((specie) => specie.name === animal.specie);
+  const animalAllSex = speciesFilter.map((specie) => ({
+    name: specie.name,
+    specieQuant: specie.residents.length,
+  }));
+  const specieSexFilter = speciesFilter.filter((specie) => specie.residents.sex === animal.sex);
+ 
+  return animalAllSex[0];
 }
 
+console.log(countAnimals({ specie: 'penguins' }));
 module.exports = countAnimals;
