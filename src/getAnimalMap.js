27d9d765d9d25ffe4aa47676da7c03a4c18c52a6
jefersongjr <jeferson_gjr@hotmail.com>
2022-04-22 15:57:02 -0300

    requisito 7 funcao quando options = undefined

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,7 +1,24 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function getAnimalMap(options) {
-  // seu código aqui
-}
+const animalNE = species.filter((animal) => animal.location === 'NE');
+const filterNE = animalNE.map((specie) => specie.name);
+const animalNW = species.filter((animal) => animal.location === 'NW');
+const filterNW = animalNW.map((specie) => specie.name);
+const animalSE = species.filter((animal) => animal.location === 'SE');
+const filterSE = animalSE.map((specie) => specie.name);
+const animalSW = species.filter((animal) => animal.location === 'SW');
+const filterSW = animalSW.map((specie) => specie.name);
 
+const undefinedRequest = (options) => ({
+  NE: filterNE,
+  NW: filterNW,
+  SE: filterSE,
+  SW: filterSW,
+});
+
+function getAnimalMap(options) {
+  return undefinedRequest(options);
+}
+console.log(getAnimalMap());
 module.exports = getAnimalMap;
