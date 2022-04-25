bc3d127e458331b2ea3f29022c0be8603dc1e986
jefersongjr <jeferson_gjr@hotmail.com>
2022-04-25 13:26:59 -0300

    criando chamada por id

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,7 +1,7 @@
 const data = require('../data/zoo_data');
 const { employees, species } = require('../data/zoo_data');
 
-function getEmployeesCoverage(employee) {
+const byName = (employee) => {
   const person = employees.find((names) => names.firstName === employee.name
   || names.lastName === employee.name);
   const animals = species.filter((specie) => person.responsibleFor.includes(specie.id));
@@ -13,8 +13,28 @@ function getEmployeesCoverage(employee) {
     species: animalName,
     locations: location,
   };
+};
+
+const byId = (employee) => {
+  const idPerson = employees.find((personId) => personId.id === employee.id);
+  const animals = species.filter((specie) => idPerson.responsibleFor.includes(specie.id));
+  const location = animals.reduce((acc, curr) => ([...acc, curr.location]), []);
+  const animalName = animals.reduce((acc, curr) => ([...acc, curr.name]), []);
+  return {
+    id: idPerson.id,
+    fullName: `${idPerson.firstName} ${idPerson.lastName}`,
+    species: animalName,
+    locations: location,
+  };
+};
+
+function getEmployeesCoverage(employee) {
+  if (employee.id !== undefined) {
+    return byId(employee);
+  }
+  return byName(employee);
 }
 
-console.log(getEmployeesCoverage({ name: 'Sharonda' }));
+console.log(getEmployeesCoverage({ id: 'c1f50212-35a6-4ecd-8223-f835538526c2' }));
 
 module.exports = getEmployeesCoverage;
